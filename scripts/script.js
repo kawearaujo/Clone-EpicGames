63e85e83f7d1d6f254d61bebcbd96db4db013e5f
kawearaujo <kawe.apr@gmail.com>
2022-04-26 12:35:57 -0300

    quase finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,30 @@
+// var indice = 1;
+// var tabcurrent_a = "1a";
+// var tabcurrent_b = "1b";
+
 $(document).ready(function(){
     let navegador = document.getElementById("topo");
     let corpo = document.getElementById("corpo");
+    
+    $(".owl-carousel").owlCarousel(({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    }));
+
+
+
     $(window).scroll(function(){
         if(this.scrollY > 60){
             navegador.classList.add("navfixed");
@@ -10,6 +34,8 @@ $(document).ready(function(){
             // corpo.style.margimTop = '50px'
         }
     });
+    
+    timer();
 });
 function changepanel(e) {
     if(e=="rl"){
@@ -32,3 +58,35 @@ $('#myList a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
   })
+
+
+// var i =0;
+// function move(tabcurrent_b) {
+//     var elem = document.getElementById(tabcurrent_b);
+//     var width = 1;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//       if (width >= 100) {
+//         clearInterval(id);
+//         i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//       }
+//     }
+//     changepanel(elem.children[0].id)
+// }
+
+// function timer() {
+//     if(indice>1){
+//         // tabcurrent_a=indice+"a";
+//         tabcurrent_b=indice+"b";
+//     }
+//     setInterval(move(tabcurrent_b), 300);
+//     if(indice => 5){
+//         indice=1;
+//     }else{
+//         indice++;
+//     };
+//     timer();
+// }
